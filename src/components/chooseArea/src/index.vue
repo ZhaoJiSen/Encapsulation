b36d81be81b8a1af9de7195f88c7ee391fa3124c
zhaojisen <10357640+zhaojisen@user.noreply.gitee.com>
2024-03-15 16:55:03 +0800

    :sparkles: 省市区三级联动组件的基本搭建

---
+++ src/components/chooseArea/src/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <el-select v-model="province" placeholder="请选择省分">
+      <el-option>1</el-option>
+    </el-select>
+    <el-select v-model="city" placeholder="请选择城市">
+      <el-option>2</el-option>
+    </el-select>
+    <el-select v-model="area" placeholder="请选择区域">
+      <el-option>3</el-option>
+    </el-select>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import type { Ref } from 'vue';
+import { ref } from 'vue';
+import allAreas from '../lib/pca-code.json';
+
+const province: Ref<string> = ref('');
+const city: Ref<string> = ref('');
+const area: Ref<string> = ref('');
+</script>
+
+<style lang="scss" scoped></style>
