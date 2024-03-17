22ea81b930470f1aad20a686fef4d3187140d45a
ZhaoJiSen <1301338853@qq.com>
2024-03-17 20:12:07 +0800

    :bookmark: 完成省市区三级联动组件

--- src/components/chooseArea/src/index.vue
+++ src/components/chooseArea/src/index.vue
@@ -1,62 +1,131 @@
 <template>
   <div>
-    <el-select v-model="province" placeholder="请选择省分" style="width: 240px">
+    <el-select
+      v-model="province"
+      placeholder="请选择省分"
+      style="width: 240px"
+      clearable
+    >
       <el-option
         v-for="item of allAreas"
         :key="item.code"
         :label="item.name"
         :value="item.code"
-        clearable
       />
     </el-select>
     <el-select
       v-model="city"
-      :disabled="!city || !province"
+      :disabled="!province"
       placeholder="请选择城市"
       clearable
       style="width: 240px; margin: 0 10px"
     >
       <el-option
-        v-for='item of selectCity'
-        :key='item.code'
-        :value='item.code'
-        :label='item.name'
+        v-for="item of selectCity"
+        :key="item.code"
+        :label="item.name"
+        :value="item.code"
       />
     </el-select>
     <el-select
       v-model="area"
-      :disabled="!area || !city"
+      :disabled="!province || !city"
       placeholder="请选择区域"
       clearable
       style="width: 240px"
     >
-      <el-option>3</el-option>
+      <el-option
+        v-for="item of selectArea"
+        :key="item.code"
+        :label="item.name"
+        :value="item.code"
+      />
     </el-select>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { Ref, ref, computed } from 'vue';
+import { watch, ref } from 'vue';
 import allAreas from '../lib/pca-code.json';
 
-const province: Ref<string> = ref('');
-const city: Ref<string> = ref('');
-const area: Ref<string> = ref('');
+export interface IArea {
+  name: string;
+  code: string;
+  children?: Array<IArea>;
+}
 
-// 2-10
-// 在选择省分后将城市过滤
-const selectCity = computed(() => {
-  let cities = [];
-  cities = allAreas.filter(area => {
-    if (area.code === province.value) {
-      return true;
+export interface IDate {
+  name: string;
+  code: string;
+}
+
+export interface IAreaOptions {
+  province: IDate;
+  city: IDate;
+  area: IDate;
+}
+
+const province = ref<string>('');
+const city = ref<string>('');
+const area = ref<string>('');
+const selectCity = ref<IArea[]>([]);
+const selectArea = ref<IArea[]>([]);
+
+const emits = defineEmits<{
+  (e: 'changeArea', {}: IAreaOptions): void;
+}>();
+
+watch(
+  () => province.value,
+  newValue => {
+    if (newValue) {
+      selectCity.value = allAreas.find(
+        item => item.code === province.value
+      )!.children;
     }
-  });
-  
-  return cities[0]?.children
-});
+    // 重新选择省份后市区与区域需要重新选择
+    city.value = '';
+    area.value = '';
+  }
+);
 
-console.log(selectCity);
+watch(
+  () => city.value,
+  newValue => {
+    if (newValue) {
+      selectArea.value = selectCity.value.find(
+        item => item.code === city.value
+      )!.children!;
+    }
+    // 重新选择市区后区域需要重新选择
+    area.value = '';
+  }
+);
+
+// 监听区域，因为区域一但有值，那么省与市一定有值
+watch(
+  () => area.value,
+  newValue => {
+    if (newValue) {
+      const provinceData: IDate = {
+        code: province.value,
+        name: allAreas.find(item => item.code === province.value)!.name
+      };
+      const cityData: IDate = {
+        code: city.value,
+        name: selectCity.value.find(item => item.code === city.value)!.name
+      };
+      const areaData: IDate = {
+        code: newValue,
+        name: selectArea.value.find(item => item.code === newValue)!.name
+      };
+
+      emits('changeArea', {
+        province: provinceData,
+        city: cityData,
+        area: areaData
+      });
+    }
+  }
+);
 </script>
-
-<style lang="scss" scoped></style>
--- src/views/chooseArea/index.vue
+++ src/views/chooseArea/index.vue
@@ -1,9 +1,13 @@
 <template>
-  <choose-area></choose-area>
+  <choose-area @change-area='changeArea'></choose-area>
 </template>
 
 <script setup lang="ts">
 import ChooseArea from '@/components/chooseArea/src/index.vue';
+
+const changeArea = (value: any) => {
+  console.log(value)
+}
 </script>
 
 <style scoped lang="scss"></style>
